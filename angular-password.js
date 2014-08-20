089c0a805b3af4d6789d6924215e5358940eb743
gdi2290 <github@gdi2290.com>
2014-08-20 12:18:32 -0700

    create mvp source

---
+++ angular-password.js
@@ -0,0 +1,34 @@
+!function(window, angular, undefined) {
+  'use strict';
+  angular.module('angular.password', [])
+  .directive('matchPassword', function() {
+
+    function link(scope, element, attrs, ctrls) {
+      var formController = ctrls[1];
+      var ngModel = ctrls[0];
+      var otherPasswordModel = formController[attrs.matchPassword];
+
+      ngModel.$parsers.push(function(value) {
+        if (value === otherPasswordModel.$viewValue) {
+          ngModel.$setValidity('passwordMatch', true);
+          return value;
+        }
+        ngModel.$setValidity('passwordMatch', false);
+      });
+
+      otherPasswordModel.$parsers.push(function(value) {
+        ngModel.$setValidity('passwordMatch', value === ngModel.$viewValue);
+        return value;
+      });
+
+    }
+    var controllers = ['^ngModel', '^form'];
+
+    return {
+      restrict: 'A',
+      require: controllers,
+      link: link
+    }; // end return
+  });
+
+}(window, window.angular);
